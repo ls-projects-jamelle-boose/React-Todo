ff12d7e32641d40e45e08dd6d0fe84edea26f4b8
user <19831942+Jamelle-Boose@users.noreply.github.com>
2019-05-18 20:02:38 -0700

    day 2 mvp complete

--- src/App.js
+++ src/App.js
@@ -4,18 +4,7 @@ import TodoForm from './components/TodoComponents/TodoForm'
 
 
 //todos array
-const todos = [
-  {
-    task: 'Organize Garage',
-    id: 1528817077286,
-    completed: false
-  },
-  {
-    task: 'Bake Cookies',
-    id: 1528817084358,
-    completed: false
-  }
-];
+const todos = []
 
 
 const defaultState = {
@@ -48,6 +37,13 @@ class App extends React.Component {
     })
   }
 
+  deleteTodoHandler = event => {
+    event.preventDefault()
+    this.setState({
+      todos: this.state.todos.filter(todo => !todo.completed)
+    })
+  }
+
   toggleTodo = todoId => {
     console.log(`todoId: ${todoId}`)
     this.setState({
@@ -75,6 +71,7 @@ class App extends React.Component {
         />
         <TodoForm 
           addTodoHandler={this.addTodoHandler}
+          deleteTodoHandler={this.deleteTodoHandler}
           changeHandler={this.changeHandler}
           task={this.state.task}
         />
